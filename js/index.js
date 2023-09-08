08b962e7a4c4f96e1ecc78ab3c8d51deaee73db2
mokhelek <mkatleho191@gmail.com>
2023-09-08 14:40:35 +0200

    add filter based on color functional

--- js/index.js
+++ js/index.js
@@ -3,6 +3,27 @@ function shoeCatalogue() {
 
         shoesList: [],
 
+        colorFilterValue:'',
+        sizeFilterValue: '',
+        brandFilterValue: '',
+
+        checkFilter(){
+            if(this.colorFilterValue != '' && this.sizeFilterValue == '' && this.brandFilterValue == '' ){
+                axios.get(`https://shoe-catalogue-api.onrender.com/api/shoes/color/${this.colorFilterValue}`).then((result) => {
+                    this.shoesList = result.data;
+                });
+            }else if(this.colorFilterValue == '' && this.sizeFilterValue != '' && this.brandFilterValue == '' ){
+                // todo -> filter by size
+            }else if(this.colorFilterValue =='' && this.sizeFilterValue == '' && this.brandFilterValue != '' ){
+                // todo -> filter by brand
+            }
+        },
+        
+        setColorFiler(color){
+            console.log(color)
+            this.colorFilterValue = color;
+        },
+
         getAllShoes() {
             axios.get("https://shoe-catalogue-api.onrender.com/api/shoes").then((result) => {
                 this.shoesList = result.data;
