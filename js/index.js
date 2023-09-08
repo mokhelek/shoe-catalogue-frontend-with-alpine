f37d01884e7d454c1662bfa6410ae90a5fa69438
mokhelek <mkatleho191@gmail.com>
2023-09-08 12:25:31 +0200

    add filter options ad dropdown menus

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
-
-function shoeCatalogue(){
-    
+function shoeCatalogue() {
     return {
-        feedback:"feedback message",
-        shoesList:[],
-        
-        getAllShoes(){
+
+        shoesList: [],
+
+        getAllShoes() {
             axios.get("https://shoe-catalogue-api.onrender.com/api/shoes").then((result) => {
                 this.shoesList = result.data;
-                console.log("Triggered ", result)
             });
         },
 
-
-        init(){
-            this.getAllShoes()
+        getShoesByBrand(brandName) {
+            axios.get(`https://shoe-catalogue-api.onrender.com/api/shoes/brand/${brandName}`).then((result) => {
+                this.shoesList = result.data;
+            });
         },
-    }
 
+        init() {
+            this.getAllShoes();
+        },
+    };
 }
