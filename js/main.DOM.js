e36f1a7cfb66f89a22a429a852da43f80aa8c7c3
mokhelek <mkatleho191@gmail.com>
2023-09-25 02:02:28 +0200

    display current selected filters

--- js/main.DOM.js
+++ js/main.DOM.js
@@ -3,3 +3,18 @@ document.addEventListener("alpine:init", () => {
     Alpine.data("shoeCatalogue", shoeCatalogue);
 });
 
+let prevColorSelection = ""
+
+function colorTest(x){
+    clearPreviousSelection(prevColorSelection);
+    let selectedColor = document.querySelector(`#${x}`)
+    selectedColor.classList.add("selected")
+    prevColorSelection = x;
+}
+
+function clearPreviousSelection(selection){
+    if(selection){
+        let selectedColor = document.querySelector(`#${selection}`)
+        selectedColor.classList.remove("selected")
+    }
+}
